d8c0e566e9cd6fa0cd4a0437422b5e5907aa9d5a
FIFA <pataradany@gmail.com>
2025-03-19 16:08:13 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // import the router
-
+import './assets/tailwind.css'; // import the tailwind css
 createApp(App)
   .use(router) // register the router
   .mount('#app')
